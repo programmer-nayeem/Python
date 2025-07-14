57addbdc03e674d315f55aed056a370ac19ca62b
Nayeem <xstarpersonal@gmail.com>
2025-07-14 07:14:24 +0600

    practice python collection

--- Collection/collection.py
+++ Collection/collection.py
@@ -7,36 +7,72 @@
 
 
 
-
+"""
 ### List !
 
-fruits = ["apple", "banana", "cherry" , "coconut"]
+fruits_list = ["apple", "banana", "cherry" , "coconut"]
 
-print(dir(fruits))  # Show all methods and attributes of the list
-print(help(fruits))  # Show help documentation for the list
-
-
+print(dir(fruits_list))  # Show all methods and attributes of the list
+print(help(fruits_list))  # Show help documentation for the list
 
 
-print(len(fruits))  # Get the number of items in the list
-for fruit in fruits:
+
+
+print(len(fruits_list))  # Get the number of items in the list
+for fruit in fruits_list:
     print(fruit)
-print("apple" in fruits)  # Check if "apple" is in the list
-fruits.append("orange")  # Add "orange" to the end of the list
-fruits.insert(1, "kiwi")  # Insert "kiwi" at index
-fruits.remove("banana")  # Remove "banana" from the list
-fruits.pop()  # Remove the last item from the list
-fruits.reverse()  # Reverse the order of the list
-fruits.sort()  # Sort the list in ascending order
-fruits.index("cherry")  # Get the index of "cherry" in the list
-fruits.count("apple")  # Count how many times "apple" appears in the list
-# fruits.clear()  # Remove all items from the list
+print("apple" in fruits_list)  # Check if "apple" is in the list
+fruits_list.append("orange")  # Add "orange" to the end of the list
+fruits_list.insert(1, "kiwi")  # Insert "kiwi" at index
+fruits_list.remove("banana")  # Remove "banana" from the list
+fruits_list.pop()  # Remove the last item from the list
+fruits_list.reverse()  # Reverse the order of the list
+fruits_list.sort()  # Sort the list in ascending order
+fruits_list.index("cherry")  # Get the index of "cherry" in the list
+fruits_list.count("apple")  # Count how many times "apple" appears in the list
+# fruits_list.clear()  # Remove all items from the list
 
 # print(fruits)
 
+"""
 
 
 
 
 
+
+
+
+
+"""
 ### Set !
+fruits_set = {"apple", "banana", "cherry", "coconut"}
+fruits_set.add("orange")  # Add "orange" to the set
+fruits_set.remove("banana")  # Remove "banana" from the set
+
+fruits_set.clear()  # Remove all items from the set
+
+print(fruits_set)
+
+"""
+
+
+
+
+
+
+
+
+#### Tuple !
+
+fruits_tuple = ("apple", "apple", "banana", "cherry", "coconut")
+print(fruits_tuple[0])  # Access the first item in the tuple
+print(fruits_tuple[1:3])  # Access a slice of the tuple
+print(fruits_tuple.index("cherry"))  # Get the index of "cherry" in the tuple
+print(fruits_tuple.count("apple"))  # Count how many times "apple" appears in
+
+for fruit in fruits_tuple:
+    print(fruit)
+
+
+# print(fruits_tuple)
